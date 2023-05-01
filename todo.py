cd5a35189d9c4d2c6b1b10ca7d3f54f74f290073
juno-choi <ililil9482@naver.com>
2023-05-01 09:21:55 +0900

    jinja2를 사용하여 template 화면 뿌리기

--- todo.py
+++ todo.py
@@ -1,38 +1,52 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, Path, status, Request, Depends
+from fastapi.templating import Jinja2Templates
 from model import Todo, TodoItem, TodoItems
 
 router = APIRouter()
 
 todo_list  = []
 
+template = Jinja2Templates(directory="templates/")
+
 @router.post("/todo")
-async def add_todo(todo: Todo):
+async def add_todo(request: Request, todo: Todo = Depends(Todo.as_form)):
+    todo.id = len(todo_list) +1
     todo_list.append(todo)
 
-    return {
-        "message" : "success"
-    }
+    return template.TemplateResponse("todo.html", 
+        {
+            "request" : request,
+            "todos" : todo_list
+        }
+    )
 
 @router.get("/todo")
-async def get_todo():
-    return {
-        "todo_list" : todo_list
-    }
+async def get_todo(request: Request):
+    return template.TemplateResponse("todo.html",
+        {
+            "request": request,
+            "todos" : todo_list
+        }
+    )
+
+
+@router.get("/todo/{todo_id}")
+async def get_todo_single(request: Request, todo_id: int = Path(..., title="The ID of the todo to retrive.")):
+    for todo in todo_list:
+        if todo.id == todo_id:
+            return template.TemplateResponse("todo.html",
+                {
+                    "request" : request,
+                    "todo" : todo
+                }
+            )
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="not found todo id"
+    )
 
 @router.get("/todo-items", response_model=TodoItems)
 async def todo_items():
     return {
         "todos" : todo_list
     }
-
-@router.get("/todo/{todo_id}")
-async def get_todo(todo_id: int):
-    for todo in todo_list:
-        if todo.id == todo_id:
-            return {
-                "todo" : todo
-            }
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail="not found todo id"
-    )
